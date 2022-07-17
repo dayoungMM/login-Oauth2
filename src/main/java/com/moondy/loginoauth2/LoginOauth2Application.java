eaac446067232d7cbd8df535e3abab3250e097fd
dayoungMM <dalbeetm@gmail.com>
2022-07-17 19:14:21 +0900

    bug: access Token 받아오는건 성공 하지만 그 후 google에 다시 요청할 때 401 에러

--- src/main/java/com/moondy/loginoauth2/LoginOauth2Application.java
+++ src/main/java/com/moondy/loginoauth2/LoginOauth2Application.java
@@ -2,6 +2,9 @@ package com.moondy.loginoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LoginOauth2Application {
--- src/main/java/com/moondy/loginoauth2/controller/IndexController.java
+++ src/main/java/com/moondy/loginoauth2/controller/IndexController.java
@@ -1,28 +1,56 @@
 package com.moondy.loginoauth2.controller;
 
-import com.moondy.loginoauth2.auth.domain.SessionUser;
+import com.moondy.loginoauth2.auth.config.SocialLoginType;
 import javax.servlet.http.HttpSession;
+import com.moondy.loginoauth2.auth.domain.GetSocialOAuthRes;
+import com.moondy.loginoauth2.auth.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/app/accounts")
 public class IndexController {
 
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
+    private final OAuthService oAuthService;
 
-    @GetMapping("/")
-    public String index(Model model) {
+//    @GetMapping("/")
+//    public String index(Model model) {
+//
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//
+//        if(user != null){
+//            model.addAttribute("username", user.getName());
+//        }
+//
+//        return "index";
+//    }
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    /**
+     * 유저 소셜 로그인으로 리다이렉트 해주는 url
+     * [GET] /accounts/auth
+     *
+     * @return void
+     */
+//    @NoAuth
+    @GetMapping("/auth/{socialLoginType}") //GOOGLE이 들어올 것이다.
+    public void socialLoginRedirect(@PathVariable(name = "socialLoginType") String SocialLoginPath) throws IOException {
+        SocialLoginType socialLoginType = SocialLoginType.valueOf(SocialLoginPath.toUpperCase());
+        oAuthService.request(socialLoginType);
+    }
 
-        if(user != null){
-            model.addAttribute("username", user.getName());
-        }
-
-        return "index";
+    @ResponseBody
+    @GetMapping(value = "/auth/{socialLoginType}/callback")
+    public GetSocialOAuthRes callback(
+            @PathVariable(name = "socialLoginType") String socialLoginPath,
+            @RequestParam(name = "code") String code) throws IOException {
+        System.out.println(">> 소셜 로그인 API 서버로부터 받은 code :" + code);
+        SocialLoginType socialLoginType = SocialLoginType.valueOf(socialLoginPath.toUpperCase());
+        GetSocialOAuthRes getSocialOAuthRes = oAuthService.oAuthLogin(socialLoginType, code);
+        return getSocialOAuthRes;
     }
 
 }
